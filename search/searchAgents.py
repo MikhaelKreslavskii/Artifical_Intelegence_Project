c9fc95c074877b49fe1732d208dbc7eaed38bfe0
Mikhael Kreslavskii <mikhaelkreslavskii@gmail.com>
2022-11-02 00:47:54 +0300

    Find the heuristic, task 6

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -407,18 +407,35 @@ def cornersHeuristic(state, problem):
     shortest path from the state to a goal of the problem; i.e.  it should be
     admissible (as well as consistent).
     """
+    
     corners = problem.corners # These are the corner coordinates
     walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
     current_position = state[0]
-    print("My current position ", current_position)
+    #print("My current position ", current_position)
     visited=state[1]
-    heuristic = manhattanHeuristic(current_position,corners[0])
-    unvisited=[]
-    for i in range(len(visited)):
+    unvisited = []
+    min_distance=0
+    #check nodes that are unvisited
+    for i in range(len(corners)):
         if visited[i]==False:
             unvisited.append(corners[i])
-    
-    return heuristic
+    #count minimal manhattan distanse between goals and choose minimal as heuristic
+    if len(unvisited)>0:
+        min_distance=util.manhattanDistance(current_position,unvisited[0])
+        min_distance_index=0
+        for i in range(len(unvisited)):
+            if util.manhattanDistance(current_position,unvisited[i])<min_distance:
+                min_distance = util.manhattanDistance(current_position,unvisited[i])
+                
+    return min_distance
+   
+    # for i in range( len(corners)):
+    #     if manhattanHeuristic(current_position,corners[i])<min_distance:
+    #         min_distance=corners[i]
+    #         min_index=i
+    # print(min_distance)
+    # print(min_index)
+   # return heuristic
     #print(corners)
     #print(walls)
 
